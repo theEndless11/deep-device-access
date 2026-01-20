fe826383641746ea2b7126adf18944d5b2cc8f40
unknown <aaa380144@gmail.com>
2026-01-20 09:16:45 +0530

    v0.0.2: Fixed some bugs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.corona.app',
+  appName: 'Corona',
+  webDir: 'www'
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { registerPlugin } from '@capacitor/core';
 import type { DeepDeviceAccessPlugin } from './definitions';
 
 const DeepDeviceAccess = registerPlugin<DeepDeviceAccessPlugin>('DeepDeviceAccess', {
-  web: () => import('./web').then((m) => new m.DeepDeviceAccessWeb()),
+  web: () => import('./web').then(m => new m.DeepDeviceAccessWeb()),
 });
 
 export * from './definitions';
